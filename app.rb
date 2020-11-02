21928d7cbe44297953d402c3ba80cf11e0ea19ff
alexcoldbrew <alexcoldbrew@gmail.com>
2020-11-02 15:56:18 -0600

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,27 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      # why does this need to be in quotes and pirates/ ?
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+  
+      # why does this need to be in quotes and pirates/ ?
+      erb :"pirates/show"
+    end
+
   end
 end
