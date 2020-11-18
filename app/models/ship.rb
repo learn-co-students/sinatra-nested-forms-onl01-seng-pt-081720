2bcd382f020c2fc2ed208925bcae6c02723a3f37
Jamal Rjoul <jamensky1991@gmail.com>
2020-11-18 21:42:47 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_reader :name, :type, :booty
+  @@all = []
+
+  def initialize(att)
+    att.each do |attribute|
+      @name = name
+      @type = type
+      @booty = booty
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.delete
+    @@all.clear
+  end
+
 end
