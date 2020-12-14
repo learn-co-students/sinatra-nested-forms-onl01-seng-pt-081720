f97e3eed1f58df7b6ec879c6fab463220b9f88a2
jazminmatos <jmatos1993@gmail.com>
2020-12-14 18:09:48 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do 
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new' #make sure to include folder that it's in
+    end
+
+    post '/pirates' do
+      #create a pirate instance
+      @pirate = Pirate.new(params[:pirate])
+
+      #create ship instances by iterating over the ship array in the pirates hash
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      #store all ships in an instance variable so that it's accessible to the view file
+      @ships = Ship.all
+
+      #where do we want this to render?
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,36 @@
 class Pirate
+    attr_reader :name, :weight, :height
+
+    @@all = []
+
+    def initialize(params)
+        @name = params[:name]
+        @weight = params[:weight]
+        @height = params[:height]
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
+
+
+#params hash
+#params
+    #pirate
+        #name
+        #weight
+        #height
+        #ships = [
+            #{name
+            #type
+            #booty
+            #},
+
+            #{name
+            #type
+            #booty
+            #}
+        #]
